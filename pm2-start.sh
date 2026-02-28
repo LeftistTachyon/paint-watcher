e8289397e4ec2299c4cd9714be9f0069c76e0373
Gui Guy <jedandeganwang1999@gmail.com>
2026-02-28 03:11:57 -0600

    Reenable autorestart

--- pm2-start.sh
+++ pm2-start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-pm2 start npm --name paint-watcher --attach --no-autorestart --time -- start
+# pm2 start npm --name paint-watcher --attach --no-autorestart --time -- start
+pm2 start npm --name paint-watcher --attach --time -- start
